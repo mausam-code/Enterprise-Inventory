aa6709de4eb431c364e8ab481a961d938783ffe6
SEASON SINGH RAJAK <seasonismausam@gmail.com>
2026-05-21 20:44:15 +0545

    Dashboard Created

---
+++ src/EnterpriseInventory.WPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EnterpriseInventory.WPF.ViewModels;
+
+public partial class BaseViewModel : ObservableObject
+{
+}
---
+++ src/EnterpriseInventory.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace EnterpriseInventory.WPF.ViewModels;
+
+public partial class MainViewModel : BaseViewModel
+{
+    public string ApplicationTitle => "Enterprise Inventory System";
+}
